265926d05807e919ec7854b2981987bcc98bad8a
Ahmednuurmohamuud <xararavic1547@gmail.com>
2025-07-12 13:02:38 +0300

    databasee chNGE

--- youthplatform/settings.py
+++ youthplatform/settings.py
@@ -112,14 +112,13 @@ WSGI_APPLICATION = 'youthplatform.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'youth_db',
-        'USER': 'root',
-        'PASSWORD': 'Raaxo9318',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': os.getenv('DB_NAME', 'youth_db'),
+        'USER': os.getenv('DB_USER', 'root'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'Raaxo9318'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
